e5502f202b3e3cee7c25a939805ce9baae1f29cc
Dave <55182282+dvsu@users.noreply.github.com>
2024-09-07 16:15:25 +0100

    Revise import

--- __init__.py
+++ __init__.py
@@ -1,4 +1,7 @@
 from .sht3x import SHT31 as SHT31
 from .sht3x import SHT35 as SHT35
+from .sht3x import Measurement as Measurement
+from .sht3x import SensorData as SensorData
+from .sht3x import SensorInfo as SensorInfo
 
-__all__ = ["SHT31", "SHT35"]
+__all__ = ["Measurement", "SensorData", "SensorInfo", "SHT31", "SHT35"]
--- sht3x/__init__.py
+++ sht3x/__init__.py
@@ -1,4 +1,7 @@
+from .models import Measurement as Measurement
+from .models import SensorData as SensorData
+from .models import SensorInfo as SensorInfo
 from .sht31 import SHT31 as SHT31
 from .sht35 import SHT35 as SHT35
 
-__all__ = ["SHT31", "SHT35"]
+__all__ = ["Measurement", "SensorData", "SensorInfo", "SHT31", "SHT35"]
